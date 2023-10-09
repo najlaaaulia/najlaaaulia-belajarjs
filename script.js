c97b1909bff6323fb316afc4048eb906762e394c
najlaaaulia <najlaaaulia318@gmail.com>
2023-10-09 15:11:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+    var x = 10;
+    var y = 2;
+
+    tambah = x + y;
+    kali = x * y;
+    kurang = x - y;
+    bagi = x / y;
+
+    document.write("x = ", x, "<br>");
+    document.write("y = ", y, "<br></br>");
+    document.write("Hasil Hitung : ", "<br>")
+    document.write("x + y = ",tambah, "<br>");
+    document.write("x * y = ",kali, "<br>");
+    document.write("x - y = ",kurang, "<br>");
+    document.write("x / y = ",bagi, "<br>");
